d8867e04c6e70174646a5d6471064b193ef4ba43
Eric Lenz <elenz@iat.tu-darmstadt.de>
2026-08-19 17:05:20 +0200

    extend result plot
    - add sigma-regions as areas around mean
    - show tag of solver used by YALMIP in legend

--- paper_ifac_2026.m
+++ paper_ifac_2026.m
@@ -41,7 +41,7 @@ for t = 0:rompc.N-1
 
     % add cost
     J = J + xt' * (sys.Q * xt) + ut' * (sys.R * ut);
-    
+
     % add continuity constraint
     Con = [Con, xt1 == sys.A * xt + sys.B * ut]; %#ok<AGROW>
     % add prestabilising constraint
@@ -78,13 +78,13 @@ for i_run = 1:n_max_run_attemps
     % define initial state & admissible guess
     % (masses two and four are displaced)
     x_init = [ 0; 1.5; 0; 1; zeros(n - 4, 1) ];
-
+    
     x_init = x_init + (0.2 * rand(12, 1) - 0.1);
-    
+
     zt = zeros(N * m, 1);
-    
+
     xt = x_init;
-    
+
     t = 0;
     Xcl = NaN(length(xt), nsteps + 1);
     Xcl(:, 1) = xt;
@@ -110,27 +110,27 @@ for i_run = 1:n_max_run_attemps
 
         if flag < 0
             fprintf('Interior point solver did not converge at time t = %d: %s\n', t, stats.msg);
-            
+
             if i_step > 1
-                error("Interior point solver couldn't find a solution for t > 0");
+                error('Interior point solver couldn''t find a solution for t > 0');
             end
 
             success = false;
             break;
         end
-    
+
         % % solve!
         tic();
         u_yal = sol_yal{xt};
         t_solve_yalmip = toc();
         solver_times(2, i_step) = t_solve_yalmip;
-    
+
         % store cost
         costs(i_step) = ut' * (sys.R * ut) + xt' * (sys.Q * xt);
 
         % apply control
         xt = sys.A * xt + sys.B * ut;
-    
+
         % store
         Xcl(:, i_step + 1) = xt;
         Ucl(:, i_step) = ut;
@@ -142,7 +142,7 @@ for i_run = 1:n_max_run_attemps
     end
 
     i_successfull_runs = i_successfull_runs + 1;
-    
+
     if i_successfull_runs > n_warmup_runs
         all_solver_times(:, :, i_successfull_runs - n_warmup_runs) = solver_times;
     end
@@ -153,17 +153,63 @@ for i_run = 1:n_max_run_attemps
 end
 
 if i_successfull_runs < n_warmup_runs + n_mpc_runs
-    error("Too many failed runs");
+    error('Too many failed runs');
 end
 
+%%
+
+% get actual solver used by yalmip
+solver_tag = regexp(evalc('sol_yal'), 'Solver:\s*(\S+)', 'tokens');
+solver_tag = solver_tag{1}{1};
+
 solver_times = mean(all_solver_times, 3);
+solver_times_std = std(all_solver_times, [], 3);
+
+tx = (0:t-1);
+
+our_mean_ms = solver_times(1, :) .* 1e3;
+yalmip_mean_ms = solver_times(2, :) .* 1e3;
+
+our_lower_ms = (solver_times(1, :) - solver_times_std(1, :)) * 1e3;
+our_upper_ms = (solver_times(1, :) + solver_times_std(1, :)) * 1e3;
+
+yalmip_lower_ms = (solver_times(2, :) - solver_times_std(2, :)) * 1e3;
+yalmip_upper_ms = (solver_times(2, :) + solver_times_std(2, :)) * 1e3;
+
+% Create vectors for manual "stairs" plot to use them with the patch
+% function. (These vectors end in "_s".)
+tx_s = reshape([tx(1:end-1); tx(2:end)], 1, []);
+
+our_lower_ms_s = kron(our_lower_ms(1:end-1), [1, 1]);
+our_upper_ms_s = kron(our_upper_ms(1:end-1), [1, 1]);
+
+yalmip_lower_ms_s = kron(yalmip_lower_ms(1:end-1), [1, 1]);
+yalmip_upper_ms_s = kron(yalmip_upper_ms(1:end-1), [1, 1]);
 
 figure();
 hold('on');
-stairs(0:t-1, solver_times(1, :) .* 1e3);
-stairs(0:t-1, solver_times(2, :) .* 1e3);
+stairs(tx, our_mean_ms);
+p = patch(...
+    'XData', [tx_s, flip(tx_s)], ...
+    'YData', [our_lower_ms_s, flip(our_upper_ms_s)], ...
+    'FaceColor', 'flat', ...
+    'EdgeColor', 'none', ...
+    'SeriesIndex', 1, ...
+    'FaceAlpha', 0.2);
+p.Annotation.LegendInformation.IconDisplayStyle = 'off';
+
+stairs(tx, yalmip_mean_ms);
+p = patch(...
+    'XData', [tx_s, flip(tx_s)], ...
+    'YData', [yalmip_lower_ms_s, flip(yalmip_upper_ms_s)], ...
+    'FaceColor', 'flat', ...
+    'EdgeColor', 'none', ...
+    'SeriesIndex', 2, ...
+    'FaceAlpha', 0.2);
+p.Annotation.LegendInformation.IconDisplayStyle = 'off';
+
 xlabel('t');
 ylabel('solvertime (ms)');
-legend({'ours', 'mosek'});
+legend({'our', solver_tag});
 ylims = ylim();
 ylim([0, ylims(2)]);
